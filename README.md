60b7264f91dd0d21a5f71a009639de26817d52a4
alexandertarakanov <alex.artoff@gmail.com>
2022-02-27 21:52:28 +0500

    add task5_toggle.py;  edit README.md, pyproject.toml

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 2. task - "count all" (`poetry run t2-count`)  
 3. task - "collect indexes" (`poetry run t3-cllcindx`)  
 4. task - "all unique" (`poetry run t4-alluniq`)
-5. task . .  
+5. task - "toggle/toggled" (`poetry run t5-toggle`)  
+6. task . . 
---
+++ block_dict/scripts/task5_toggle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+from copy import copy
+
+
+def toggle(flg, st):
+    if flg in st:
+        st.discard(flg)
+    else:
+        st.add(flg)
+
+
+def toggled(flg, st):
+    new_st = st.copy()
+    toggle(flg, new_st)
+    return new_st
+
+
+def main():
+    print(toggle('r', {'-', 'r', 'w', 'e'}))
+    print(toggled('r', {'-', 'r', 'w', 'e'}))
+
+
+if __name__ == "__main__":
+    main()
